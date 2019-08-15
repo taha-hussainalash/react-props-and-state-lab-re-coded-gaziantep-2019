2e155799511906c684529ed6bb4c5348d337db9d
Taha HussainAlash <taha.benzma94@gmail.com>
2019-08-15 19:32:10 +0000

    Automatically backed up by Learn

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -14,7 +14,7 @@ class Pet extends React.Component {
           </div>
           <div className="description">
             <p>Age:  {this.props.pet.age}</p>
-            <p>Weight:</p>
+            <p>Weight:{this.props.pet.weight}</p>
           </div>
         </div>
         <div className="extra content">
